056d805fc52f3ac17c4d4a840078f917355c60bd
Lionel EKOM <Lionelbieko@gmail.com>
2023-09-07 02:39:53 +0200

    Creation de l'entite User et CRUD

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+            ])
+            ->add('FirstName')
+            ->add('LastName')
+            ->add('Sexe')
+            ->add('Fonction')
+            ->add('Telephone')
+            ->add('Title')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
