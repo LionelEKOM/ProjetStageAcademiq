faaaf8f02816ea107d528fb3b3be106a20374386
Lionel EKOM <Lionelbieko@gmail.com>
2023-09-19 08:38:29 +0100

    customized consultation form

--- src/Form/DossierType.php
+++ src/Form/DossierType.php
@@ -40,25 +40,27 @@ class DossierType extends AbstractType
 $builder
     ->add('firstname', TextType::class, [
         'attr' => ['class' => 'form-control', 'placeholder' => 'Prénom'],
-        'label' => 'Prénom',
+        'label' => 'Prénom Patient',
         'constraints' => [
             new NotBlank(['message' => 'Veuillez entrer votre Prénom.']),
         ],
     ])
     ->add('lastname', TextType::class, [
         'attr' => ['class' => 'form-control', 'placeholder' => 'Nom'],
-        'label' => 'Nom',
+        'label' => 'Nom Patient',
         'constraints' => [
             new NotBlank(['message' => 'Veuillez entrer votre Nom.']),
         ],
     ])
     ->add('age', IntegerType::class, [
+        'attr' => ['placeholder' => 'Age'],
         'constraints' => [
             new NotBlank(['message' => 'L\'âge ne peut pas être vide.']),
             new Range(['min' => 1, 'max' => 102, 'minMessage' => 'L\'âge doit être d\'au moins {{ limit }}.', 'maxMessage' => 'L\'âge ne peut pas dépasser {{ limit }}.']),
         ],
     ])
     ->add('sexe', ChoiceType::class, [
+        'attr' => ['placeholder' => 'Sexe'],
         'choices' => [
             'Homme' => 'Homme',
             'Femme' => 'Femme',
@@ -68,34 +70,40 @@ $builder
         ],
     ])
     ->add('email', EmailType::class, [
+        'attr' => ['placeholder' => 'E-mail'],
         'constraints' => [
             new NotBlank(['message' => 'L\'adresse e-mail ne peut pas être vide.']),
             new Email(['message' => 'Veuillez entrer une adresse e-mail valide.']),
         ],
     ])
     ->add('adresse', TextType::class, [
+        'attr' => ['placeholder' => 'Adresse'],
         'constraints' => [
             new NotBlank(['message' => 'L\'adresse ne peut pas être vide.']),
         ],
     ])
     ->add('phone', TelType::class, [
+        'attr' => ['placeholder' => 'Numero de téléphone'],
         'constraints' => [
             new NotBlank(['message' => 'Le numéro de téléphone ne peut pas être vide.']),
             new Regex(['pattern' => '/^6\d{8}$/', 'message' => 'Veuillez entrer un numéro de téléphone valide.']),
         ],
     ])
     ->add('profession', TextType::class, [
+        'attr' => ['placeholder' => 'Profession'],
         'constraints' => [
             new NotBlank(['message' => 'La profession ne peut pas être vide.']),
         ],
     ])
     ->add('taille', NumberType::class, [
+        'attr' => ['placeholder' => 'Taille'],
         'constraints' => [
             new NotBlank(['message' => 'La taille ne peut pas être vide.']),
             new Range(['min' => 1, 'max' => 3.03, 'minMessage' => 'La taille doit être d\'au moins {{ limit }}.', 'maxMessage' => 'La taille ne peut pas dépasser {{ limit }}.']),
         ],
     ])
     ->add('poids', NumberType::class, [
+        'attr' => ['placeholder' => 'Poids'],
         'constraints' => [
             new NotBlank(['message' => 'Le poids ne peut pas être vide.']),
             new Range(['min' => 2, 'max' => 110, 'minMessage' => 'Le poids doit être d\'au moins {{ limit }}.', 'maxMessage' => 'Le poids ne peut pas dépasser {{ limit }}.']),
